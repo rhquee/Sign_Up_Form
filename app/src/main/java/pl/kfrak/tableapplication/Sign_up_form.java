1a932a7f390d2b2f289e79619da7883161a17c58
rhquee <kfrak@proarray.com>
2017-06-12 20:50:55 +0200

    Initial commit

---
+++ app/src/main/java/pl/kfrak/tableapplication/Sign_up_form.java
@@ -0,0 +1,14 @@
+package pl.kfrak.tableapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_up_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_form);
+
+    }
+}
